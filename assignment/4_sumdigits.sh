2245cac5b8997a8f03cd1d8124a7b7802ff240d7
farazkhanfk7 <farazkhan138@gmail.com>
2022-05-24 18:57:12 +0530

    upto 21

--- assignment/4_sumdigits.sh
+++ assignment/4_sumdigits.sh
@@ -6,9 +6,9 @@ sum=0
 
 while [ $num -gt 0 ]
 do
-    mod=$((num % 10))    #It will split each digits
-    sum=$((sum + mod))   #Add each digit to sum
-    num=$((num / 10))    #divide num by 10.
+    mod=$((num % 10)) 
+    sum=$((sum + mod)) 
+    num=$((num / 10)) 
 done
 
 echo $sum
